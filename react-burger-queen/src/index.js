313b53b6db5795218763982fa4117fcc1834eaad
melizasosa <s.m.meliza@gmail.com>
2019-10-09 10:47:04 -0500

    Eliminando archivo de src y hola mundo con react y jsx

--- react-burger-queen/src/index.js
+++ react-burger-queen/src/index.js
@@ -1,13 +1,14 @@
+// COMO SOLIAMOS HACER
+// const element = document.createElement('h1');
+// element.innerText = 'Hola mundo';
+// const container =document.getElementById('root');
+// container.appendChild(element);
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// CON JSX
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
-serviceWorker.unregister();
+const element = <h1> Hola Mundo</h1>
+const container = document.getElementById('root');
+ReactDom.render(element, container);
