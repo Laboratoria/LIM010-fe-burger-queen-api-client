2f220b1632b8f7a0d61cb9866f3772cd0d6d7c5b
zaidaKary <zaida.260194@gmail.com>
2019-10-09 19:17:56 -0500

    Subiendo carpetas organizadas

---
+++ burger-queen/src/components/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/Content.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="Content">
+                <h1>Soy el Contenido</h1>
+
+                <p>Lorem impsum...</p>
+            </div>
+        );
+    }
+ }
+
+export default Content;
