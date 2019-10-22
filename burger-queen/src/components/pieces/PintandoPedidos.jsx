b428f572ba9b355c4c9afe34f1a0f11b72d4fc2a
melizasosa <s.m.meliza@gmail.com>
2019-10-22 16:55:14 -0500

    Practicando useState y useEffect

--- burger-queen/src/components/pieces/PintandoPedidos.jsx
+++ burger-queen/src/components/pieces/PintandoPedidos.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import products from '../controllers/products.js';
 
 // const [prodData, setProdData] = useState([]);
 const PintarProductos = () => {
+  const [prodData, setProdData] = useState([]);
+  const [prodType, setProdType] = useState('desayuno');
+
   const productos = (token) => {
-    products(token).then((res) => (
-      res.productos.map((item) => console.log(item.name),
-        //   <button key={item.id}>{item.name}</button>
-      )
-    )).catch((error) => {
+    products(token).then((res) => {
+     setProdData(res.products); 
+    }).catch((error) => {
       console.log(error);
     });
   };
+
+  useEffect(() => {
+    productos('el token')
+  }, [])
+  // const getProduct = (e) =>{
+  //   setProdData(e);
+  // } 
+
   return (
     <div>
-      <button type="submit" onClick={productos}>Productos</button>
+      <button
+        type="submit"
+        onClick={() => {
+          setProdType('desayuno')
+        }}>
+          Desayuno
+      </button>
+      <button
+        type="submit"
+        onClick={() => {
+          setProdType('almuerzo')
+        }}>
+          Almuerzo
+      </button>
+      {prodData.filter(p => p.type === prodType).map((p) => (
+        <button key={p.id}>{p.name}</button>
+      ))}
     </div>
   );
 };
