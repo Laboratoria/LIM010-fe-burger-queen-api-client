43d2d9545fb3ca78088cacc7039cb83684b28a33
zaidaKary <zaida.260194@gmail.com>
2019-10-24 18:19:37 -0500

    Boton eliminar completado

--- burger-queen/src/components/controllers/eliminarProducto.js
+++ burger-queen/src/components/controllers/eliminarProducto.js
@@ -2,6 +2,6 @@ const deleteProduct = (arrayOrder, item) => {
   // console.log(arrayOrder, item);
   const newProductos = arrayOrder.filter((p) => p.id !== item);
   return newProductos;
-  };
+};
 
 export default deleteProduct;
