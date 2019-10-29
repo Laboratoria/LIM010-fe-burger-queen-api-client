51c3a946d679906edc63d3c231358956b228efe4
melizasosa <s.m.meliza@gmail.com>
2019-10-29 17:26:53 -0500

    Vista de lista pedidos mesero

--- burger-queen/src/components/pages/ViewWaiterMenu.jsx
+++ burger-queen/src/components/pages/ViewWaiterMenu.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import PHeader from '../pieces/PHeader.jsx';
 import PFooter from '../pieces/PFooter.jsx';
-// import PMenu from '../pieces/PMenu.jsx';
-// import PintandoPedidos from '../pieces/PintandoPedidos.jsx';
-// import ViewWaiterListPedidos from './ViewWaiterListPedidos.jsx';
-// import PintandoListPedidos from '../pieces/PintandoListPedidos.jsx';
-// import ListMenu from './ListMenu.jsx';
-// import PintandoOrders from '../pieces/PintandoOrders.jsx';
+
 import FilterStatusOrder from '../pieces/statusOrderKitchen.jsx';
 
 const ViewWaiterMenu = () => (
